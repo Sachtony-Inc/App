f11f9fe58922fd76959ef7dd970585c6477e1ab7
Sachtony Company Limited <51844180+sachtonysoft@users.noreply.github.com>
2019-12-31 12:28:57 +0700

    Update Products.js

--- sachtonyApp/www/scripts/Products.js
+++ sachtonyApp/www/scripts/Products.js
@@ -1,1 +1,11 @@
-﻿
+<!-- Sachtony Store -->
+
+        <script type='text/javascript'>
+        var ccs_cc_args = ccs_cc_args || [];
+        (function () {
+        var o = ccs_cc_args; o.push(['_SKey', '0e0f3f20']); o.push(['_ZoneId', 'a28c96fabf']); 
+        var sc = document.createElement('script'); sc.type = 'text/javascript'; sc.async = true;
+        sc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.cnetcontent.com/jsc/h.js';
+        var n = document.getElementsByTagName('script')[0]; n.parentNode.insertBefore(sc, n);
+        })();
+        </script>
